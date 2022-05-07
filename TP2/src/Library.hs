3373800824b5210f40c06d012667e1f1aaefbc29
Brunosaurio19 <bdeangelis@frba.utn.edu.ar>
2022-05-07 20:36:05 -0300

    agrego punto 3

--- TP2/src/Library.hs
+++ TP2/src/Library.hs
@@ -14,13 +14,15 @@ data Persona = UnaPersona {
 
 
 bruno = UnaPersona {nombre = "Bruno", edad = 25, stress = 10, preferencias = ["voley", "prima de ludmi", "comida"], amigues = ["Walter","Ludmi"]}
-walter = UnaPersona "Walter" 41 52 [] ["Kjiro"]
+walter = UnaPersona "Walter" 41 40 [] []
 ringo = UnaPersona "Rigoberta" 31 60 [] ["Stacy"]
+tony = UnaPersona "Antonio" 25 75 [] []
+juancho = UnaPersona "Juan" 37 80 [] [] 
 --Punto1--
 
 amigospar persona = even (length (amigues persona))
 
-scoring persona | amigospar persona = (edad persona) * (stress persona)
+scoring persona  | amigospar persona = (edad persona) * (stress persona)
                 | edad persona > 40 = length (amigues persona) * (edad persona)
                 |otherwise = length (nombre persona) * 2 
 
@@ -33,3 +35,14 @@ nombreFirme persona = head (nombre persona) == 'p'
 personaInteresante persona = length (amigues persona) >= 2
 
 --Punto3--
+
+mes x = x <= 2     --Devuelve True si el mes es menor o igual que dos
+plata x = x == 0   --Devuel true si va SIN plata
+
+marDelPlata parametro persona   | mes parametro = persona {stress = (stress persona) + 10}
+                                |otherwise = persona {stress = max ((stress persona) - min (edad persona) 20) 0}
+
+lasToninas parametro persona | plata parametro = marDelPlata 5 persona
+                             |otherwise = persona {stress = (stress persona)/2}
+
+puertoMadryn persona = persona {stress = 0, amigues = (amigues persona) ++ ["Juan"] }                    
